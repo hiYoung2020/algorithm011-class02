2e1438448dbad3895842742fbdf9a9e4c6221a85
zhaohaiyang02 <zhaohaiyang02@58.com>
2020-08-23 17:51:14 +0800

    no message

---
+++ Week_09/[541]反转字符串 II.java
@@ -0,0 +1,45 @@
+//给定一个字符串 s 和一个整数 k，你需要对从字符串开头算起的每隔 2k 个字符的前 k 个字符进行反转。 
+//
+// 
+// 如果剩余字符少于 k 个，则将剩余字符全部反转。 
+// 如果剩余字符小于 2k 但大于或等于 k 个，则反转前 k 个字符，其余字符保持原样。 
+// 
+//
+// 
+//
+// 示例: 
+//
+// 输入: s = "abcdefg", k = 2
+//输出: "bacdfeg"
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 该字符串只包含小写英文字母。 
+// 给定字符串的长度和 k 在 [1, 10000] 范围内。 
+// 
+// Related Topics 字符串
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public String reverseStr(String s, int k) {
+        if (s == null || s.isEmpty()) return s;
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i += 2 * k) {
+            int start = i;
+            int end = Math.min(i + k - 1, s.length() - 1);
+            while (start < end) {
+                char temp = chars[start];
+                chars[start++] = chars[end];
+                chars[end--] = temp;
+            }
+        }
+        return new String(chars);
+    }
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
